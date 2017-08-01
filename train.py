2e6a403443c42fc3df7388e8fb94f7b0f003d72c
Insik Kim <insik92@gmail.com>
2017-08-01 21:11:19 +0900

    save, load model weights.

--- train.py
+++ train.py
@@ -14,10 +14,11 @@ from trainer import Trainer
 flags = tf.app.flags
 
 
-TRAIN_DATA_DIR = 'train_output/'
-CHECKPOINT_FILE = 'model.ckpt'
-CHECKPOINT_FILE_PATH = os.path.join(TRAIN_DATA_DIR, CHECKPOINT_FILE)
 
+
+flags.DEFINE_string("train_dir", "train_output", "training output directory. checkpoints, logs are saved in here.")
+
+flags.DEFINE_integer("load_step", 0, "load model weight at the given global step [0]")
 flags.DEFINE_integer("num_epochs", 100, "Num Epochs [100]")
 flags.DEFINE_integer("batch_size", 100, "Batch size [100]")
 flags.DEFINE_integer("num_classes", 2, "Num classes [2]")
@@ -28,7 +29,21 @@ NUM_TRAIN_EXAMPLES = read_data.NUM_TRAIN_EXAMPLES
 NUM_VALIDATION_EXAMPLES = read_data.NUM_VALIDATION_EXAMPLES
 
 # TODO: add debug flag so we can debug code without waiting for epoch.
-
+def restore(config, sess):
+     """restore from the previous checkpoint"""
+    try:
+        checkpoint_path = tf.train.latest_checkpoint(config.train_dir)
+        restorer = tf.train.Saver()
+        restorer.restore(sess, checkpoint_path)
+        print ('restored previous model %s from %s'\
+                %(checkpoint_path, config.train_dir))
+        time.sleep(2)
+        return
+    except:
+        print ('--restore_previous_if_exists is set, but failed to restore in %s %s'\
+                % (config.train_dir, checkpoint_path))
+        print('Start from new weights')
+        time.sleep(2)
 
 def run_training(config):
     print("config.num_epochs:", config.num_epochs)
@@ -62,16 +77,25 @@ def run_training(config):
 
     summary_op = tf.summary.merge_all()
 
-    ## Saver
+    tf_global_step = slim.get_or_create_global_step()
 
-    saver = tf.train.Saver(tf.all_variables(), max_to_keep=1)
-
+    # Load from previous one
+    if config.load_step > 0:
+        save_path = os.path.join(config.train_dir, "{}-{}".format('model.ckpt', config.load_step))
+        print("Loading saved model from {}".format(save_path))
+        saver = tf.train.Saver()
+        saver.restore(sess, save_path)
+    else:
+        restore(sess)
     
     ## Init a model
     init_op = tf.group(tf.initialize_all_variables(), tf.initialize_local_variables())
 
     sess = tf.Session()
     sess.run(init_op)
+
+    ## Saver
+    saver = tf.train.Saver(tf.all_variables(), max_to_keep=5)
 
     ## Start input queue
     coord = tf.train.Coordinator()
@@ -95,20 +119,22 @@ def run_training(config):
 
             feed_dict = m.get_feed_dict(batch_images, batch_labels)
 
-            _, loss_cls, loss_reg = sess.run([train_op, m.loss_cls, m.loss_reg], feed_dict=feed_dict)
+            global_step, _, loss_cls, loss_reg = sess.run([tf_global_step, train_op, m.loss_cls, m.loss_reg], feed_dict=feed_dict)
+            global_step += 1
             tot_loss = loss_cls + loss_reg
             duration = time.time() - start_time
             assert not np.isnan(tot_loss), 'Model diverged with loss = NaN'
 
             if step % 10 == 0:
-                print('Step %d : loss_cls = %.5f, loss_reg = %.5f, loss_tot = %.5f (%.3f sec)'
-                        % (step, loss_cls, loss_reg, tot_loss, duration))
+                print('GlobalStep %d : loss_cls = %.5f, loss_reg = %.5f, loss_tot = %.5f (%.3f sec)'
+                        % (global_step, loss_cls, loss_reg, tot_loss, duration))
                 summary_str = sess.run(summary_op, feed_dict=feed_dict)
                 summary_writer.add_summary(summary_str, step)
 
             if step % num_iter_per_epoch == 0 and step > 0: # Do not save for step 0
                 num_epochs = int(step / num_iter_per_epoch)
-                saver.save(sess, CHECKPOINT_FILE_PATH, global_step=step)
+                CHECKPOINT_FILE_PATH = os.path.join(config.train_dir, 'model.ckpt')
+                saver.save(sess, CHECKPOINT_FILE_PATH, global_step=global_step)
                 print("Start Periodical Evaluation with Validation Set on Training Graph")
                 sum_loss = 0.0
                 sum_acc = 0.0
